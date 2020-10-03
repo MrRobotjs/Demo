5bad6fc7d059a3bd58a8b618c8bac1790941d4b1
Christopher <christopher.00.ramos@gmail.com>
2020-10-02 17:49:49 -0700

    Revert "update"
    This reverts commit e81b8cf644a1b0ede991c128d88acdbd01bf2079.

--- gatsby-node.js
+++ gatsby-node.js
@@ -10,10 +10,12 @@ exports.onCreateNode = async ({
   }) => {
   const { createNode } = actions
   
+  let productImages = node.Media
+  
   if (node.internal.type === "StrapiProducts") {
-    if (node.Media.length > 0) {
-      const ProductImages = await Promise.all(
-        node.Media.map(el =>
+    if (productImages.length > 0) {
+      const images = await Promise.all(
+      productImages.map(el =>
           createRemoteFileNode({
             url: `${el.url}`,
             parentNodeId: node.id,
@@ -25,16 +27,24 @@ exports.onCreateNode = async ({
         )
       )
   
-      node.Media.forEach((node, i) => {
-        node.localFile___NODE = ProductImages[i].id
+      productImages.forEach((node, i) => {
+        node.localFile___NODE = images[i].id
       })
     }
+  }
 
+  if (node.internal.type === "StrapiProducts") {
     if (node.ColorVariant.length > 0) {
       // Map ColorVariants Objects (2 in Total)
       const VariantMedia = node.ColorVariant.map(e => {
         return e
       })
+      // Returns total number of objects inside VariantMedia (2)
+      let TotalCount = _.size(VariantMedia)
+      for(var i = 0; i< TotalCount; i++){
+        var Total = i;
+        console.log(Total);
+      }
 
       //Returns Array of Objects
       console.log(VariantMedia);
@@ -42,7 +52,7 @@ exports.onCreateNode = async ({
       //Returns the ID of the FIRST Media[0] inside FIRST VariantMedia[0]
       console.log(VariantMedia[0].Media[0].id + " - log")
 
-      const ColorVariantImages = await Promise.all(
+      const images2 = await Promise.all(
         
         //Maps Media from the FIRST Object from VariantMedia[0]
         VariantMedia[0].Media.map(el2 =>
@@ -58,8 +68,8 @@ exports.onCreateNode = async ({
         
       )
       
-      //Returns result of ColorVariantImages Promise in console
-      console.log(ColorVariantImages)
+      //Returns result of images2 Promise in console
+      console.log(images2)
       
       VariantMedia.forEach((node, i) => {
         console.log("Product - " + i)
@@ -70,10 +80,9 @@ exports.onCreateNode = async ({
 
       //Gives localfile to Media in the FIRST object[0]
       VariantMedia[0].Media.forEach((node, i) => {
-        node.localFile___NODE = ColorVariantImages[i].id
+        node.localFile___NODE = images2[i].id
       })
 
     }
-
   }
 }
